0f81b03591869c28c0eb9dce242736118a774598
Joe Koning <koning@users.noreply.github.com>
2016-10-04 10:47:59 -0700

    Fix get _imu_values
    Set actual call command and update the array length checking.

--- ros_arduino_python/src/ros_arduino_python/arduino_serial.py
+++ ros_arduino_python/src/ros_arduino_python/arduino_serial.py
@@ -310,13 +310,12 @@ class ArduinoSerial(Arduino):
         return self.execute('z %d %d' %(triggerPin, outputPin))
 
     def get_imu_values(self):
-        values = self.execute_array('e', vtype=float)
-        if len(values) != 3:
-            print "IMU count was not 3"
-            raise SerialException
-            return None
-        else:
-            return values
+        '''Get the values from the IMU
+        Different IMUs will have different number of returned values so
+        the length should be check in the concrete IMU object.
+        '''
+        values = self.execute_array('i', vtype=float)
+
 
 """ Basic test for connectivity """
 if __name__ == "__main__":
